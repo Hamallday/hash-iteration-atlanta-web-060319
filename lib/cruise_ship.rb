c710a2edfc94dcc05710eb50b1df8c181dcdb196
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 22:09:13 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,20 +7,13 @@
 # }
 
 def select_winner
-  winner = ""
+  winner = []
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
+      winner << name
     end
-  end
 
   winner
 
 end
-
-def happy_birthday(birthday_kids)
-
-  birthday_kids.each do |kids_name,age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
 end
